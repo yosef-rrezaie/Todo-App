2546388e0890c41906cc8492cf8a8111e9db4ea8
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-04-01 04:15:20 +0330

    add back and next button to change status

--- components/layout/modules/Task.js
+++ components/layout/modules/Task.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { RiMastodonLine } from "react-icons/ri";
 
-function Task({ data }) {
+function Task({ data, next, back }) {
   return (
     <div className="tasks">
       {data.map((i) => (
         <div key={i._id} className="tasks__card">
           <span className={i.status}></span>
-          <RiMastodonLine/>
+          <RiMastodonLine />
           <h4>{i.title}</h4>
+          <div>
+            {back ? (<button className="button-back">Back</button>) : null }
+            {next ? (<button className="button-next">Next</button>) : null }
+          </div>
         </div>
       ))}
     </div>
--- components/layout/templates/HomePage.js
+++ components/layout/templates/HomePage.js
@@ -9,19 +9,19 @@ function HomePage({ data }) {
     <div className="home-page">
       <div className="home-page--todo">
         <p>Todo</p>
-        {todos.todo && <Task data={todos.todo} />}
+        {todos.todo && <Task data={todos.todo} next="inProgress" />}
       </div>
       <div className="home-page--inProgress">
         <p>In Progress</p>
-        {todos.inProgress && <Task data={todos.inProgress} />}
+        {todos.inProgress && <Task data={todos.inProgress} next="review" back="todo" />}
       </div>
       <div className="home-page--review">
         <p>Review</p>
-        {todos.review && <Task data={todos.review} />}
+        {todos.review && <Task data={todos.review} next="done" back="inProgress" />}
       </div>
       <div className="home-page--done">
         <p>Done</p>
-        {todos.done && <Task data={todos?.done} />}
+        {todos.done && <Task data={todos?.done} back="review" />}
       </div>
     </div>
   );
